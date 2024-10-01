08bb231f3ba0a98ee2ea4e3ea63e8c6b83c530b7
Cheng-Sung Tsai <cdlab.ice.cycu@gmail.com>
2024-10-01 16:21:23 +0800

    code

---
+++ code/test/LotOfAdd.c
@@ -0,0 +1,20 @@
+#include "syscall.h"
+
+int main() {
+    int result;
+
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+    result = Add(42, 23);
+
+    Halt();
+    /* not reached */
+}
---
+++ code/test/build_nachos.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# build nachos
+cd ../build.linux
+make clean > /dev/null 2>&1
+make -j > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "Build success"
+else
+    echo "Build failed"
+    exit 1
+fi
